b8ac520acb599c91c37fb97beb9b4a602e224733
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2021-07-19 04:10:42 +0700

    cm17

--- bookStory.WebBookApp/wwwroot/js/signalrchat.js
+++ bookStory.WebBookApp/wwwroot/js/signalrchat.js
@@ -10,7 +10,7 @@
         var list = '';
         var unnn_array = new Array();
         var dem = 0;
-        var un2first = '';
+        var array_id = new Array();
         $.ajax({
             url: '/vi/Chat/GetChat',
             method: 'GET',
@@ -27,86 +27,19 @@
                         }
                         if (kt == false) {
                             unnn_array[dem] = username2;
+                            array_id[dem] = v.Id;
                             dem++;
-                            //if (dem == 1) {
-                            //    list += `
-                            //    <li  class="messaging-member messaging-member--active">
-                            //   <div class="messaging-member__wrapper">
-                            //        <div class="messaging-member__avatar">
-                            //            <img src="https://randomuser.me/api/portraits/thumb/women/17.jpg" alt="Courtney Simmons" loading="lazy">
-                            //            <div class="user-status"></div>
-                            //        </div>
-                            //        <span class="messaging-member__name">${v.FirstName2} ${v.LastName2}</span>
-                            //        <span class="messaging-member__message">${v.Message}</span>
-                            //    </div>
-                            //    </li>
-                            //    `
-                            //} else {
-                            //    list += `
-                            //    <li  class="messaging-member">
-                            //   <div class="messaging-member__wrapper">
-                            //        <div class="messaging-member__avatar">
-                            //            <img src="https://randomuser.me/api/portraits/thumb/women/17.jpg" alt="Courtney Simmons" loading="lazy">
-                            //            <div class="user-status"></div>
-                            //        </div>
-                            //        <span class="messaging-member__name">${v.FirstName2} ${v.LastName2}</span>
-                            //        <span class="messaging-member__message">${dem} ${v.Message}</span>
-                            //    </div>
-                            //    </li>
-                            //    `
-                            //}
                         }
                     }
                 })
-
-                un2first = unnn_array[0];
-                $("#listReceiver").html(list);
             },
             error: (error) => {
                 console.log(error)
             }
         })
-        //var li = '';
-        //var unsender = document.getElementById("unsender").value.toString();
         var arr = [5, 8, 9, 10, 11];
         for (var i = 0; i < 5; i++) {
-            //var tinnhan = "#tinnhan" + arr[i].toString();
-            //var li = `${tinnhan}`;
-            //var lii = '';
             callchat(arr[i]);
-            //$.ajax({
-            //    url: '/vi/Chat/GetChat',
-            //    method: 'GET',
-            //    success: (result) => {
-            //        $.each(result, (k, v) => {
-            //            lii += `*/${v.Id}`
-
-            //            //if (v.UserName1 == unsender && v.UserName2 == un2first) {
-            //            //    li += `
-            //            //       <div class="chat__bubble chat__bubble--you">
-            //            //                    ${v.Message}
-            //            //       </div>
-            //            //        `
-            //            //}
-            //            //if (v.UserName1 == un2first && v.UserName2 == unsender) {
-            //            //    li += `
-            //            //       <div class="chat__bubble chat__bubble--me">
-            //            //            ${v.Message}
-            //            //        </div>
-            //            //        `
-            //            //}
-            //            //$(tinnhan).html(lii);
-            //        })
-
-            //        //var lii = `tinhtan`;
-            //        //var tinhtan = "#chatchat123"
-            //        //$(tinhtan).html(lii);
-            //    },
-            //    error: (error) => {
-            //        console.log(error)
-            //    }
-            //})
-            //$(tinnhan).html(li);
         }
         function callchat(id) {
             var tinnhan = "#tinnhan" + id.toString();
